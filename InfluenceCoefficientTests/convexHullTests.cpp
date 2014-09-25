267efbed5465ea17223b5d2be8b65eab35a56bb5
csatterwhite <chris.satterwhite@me.com>
2014-09-25 11:21:58 -0700

    Test suite for convex hull implementation using Graham scan algorithm.

---
+++ InfluenceCoefficientTests/convexHullTests.cpp
@@ -0,0 +1,9 @@
+//
+//  convexHullTests.cpp
+//  CPanel
+//
+//  Created by Chris Satterwhite on 9/24/14.
+//  Copyright (c) 2014 Chris Satterwhite. All rights reserved.
+//
+
+#include "convexHullTests.h"
---
+++ InfluenceCoefficientTests/convexHullTests.h
@@ -0,0 +1,14 @@
+//
+//  convexHullTests.h
+//  CPanel
+//
+//  Created by Chris Satterwhite on 9/24/14.
+//  Copyright (c) 2014 Chris Satterwhite. All rights reserved.
+//
+
+#ifndef __CPanel__convexHullTests__
+#define __CPanel__convexHullTests__
+
+#include <stdio.h>
+
+#endif /* defined(__CPanel__convexHullTests__) */
