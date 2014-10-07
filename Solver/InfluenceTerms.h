e28ea0bb6b2ec2fe53060409790f81d7e93eb154
csatterwhite <chris.satterwhite@me.com>
2014-10-07 11:06:21 -0700

    Added structure that contains the terms needed in influence calculations for both source and doublet panels.  Terms found in Katz and Plotkin page 246 and based on geometry of panel and the point of interest.

---
+++ Solver/InfluenceTerms.h
@@ -0,0 +1,22 @@
+//
+//  InfluenceTerms.h
+//  CPanel
+//
+//  Created by Chris Satterwhite on 10/6/14.
+//  Copyright (c) 2014 Chris Satterwhite. All rights reserved.
+//
+
+#ifndef __CPanel__InfluenceTerms__
+#define __CPanel__InfluenceTerms__
+
+#include <stdio.h>
+#include <Eigen/Dense>
+
+struct influenceTerms
+{
+    Eigen::VectorXd d,m,r,e,h;
+    influenceTerms(const Eigen::MatrixXd &verts, const Eigen::Vector3d &POI);
+
+};
+
+#endif /* defined(__CPanel__InfluenceTerms__) */
